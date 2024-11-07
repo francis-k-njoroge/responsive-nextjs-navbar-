b39388677f6176b43facffe12e3758dfcae8b59d
francis-k-njoroge <85516786+francis-k-njoroge@users.noreply.github.com>
2024-11-08 01:18:42 +0300

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#81C408",
+        secondary: "#ffb524",
+        light: "#FFFFFF",
+        dark: "#000000",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
